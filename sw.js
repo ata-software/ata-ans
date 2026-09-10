d5e3851d164580dbfa1c6cc20b3e966ab6e581be
ATA <265379470+ata-software@users.noreply.github.com>
2026-09-10 12:01:19 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "ata-pwa-v21";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./avatar.png",
+    "./avatar_backup.png",
+    "./logo.png",
+    "./manifest.json"
+];
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((k) => {
+                    if (k !== CACHE_NAME) return caches.delete(k);
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
